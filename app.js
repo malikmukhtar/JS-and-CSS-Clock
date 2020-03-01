2435524cc4c4ce5cc2acd6b1b17c61e07ac487b8
Malik <malikmukhtar63@gmail.com>
2020-03-01 18:20:32 +0100

    JSS CSS Clock

---
+++ app.js
@@ -0,0 +1,29 @@
+
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+
+
+const setDate = () => {
+    
+    const now = new Date();
+
+
+    const seconds = now.getSeconds();    
+    const secondDegree = ((seconds / 60) * 360) + 90;    
+    secondHand.style.transform = `rotate(${secondDegree}deg)`
+    
+  
+
+    const minutes = now.getMinutes();
+    const minuteDegree = ((minutes / 60) * 360) + 90;
+    minuteHand.style.transform = `rotate(${minuteDegree}deg)`
+
+    const hours = now.getHours()
+    const hourDegree = ((hours / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hourDegree}deg)`
+
+}
+
+setInterval(setDate, 1000)
